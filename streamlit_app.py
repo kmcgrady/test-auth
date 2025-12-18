e738819cae0c25a7f873686a69c482d1f894d175
Ken McGrady <ken.mcgrady@snowflake.com>
2025-12-18 08:45:26 -0800

    Initial commit

---
+++ streamlit_app.py
@@ -0,0 +1,14 @@
+import streamlit as st 
+
+if not st.user.is_logged_in:
+    st.header("This app is private.")
+    st.subheader("Please log in.")
+    st.button("Log in with Google", on_click=st.login)
+else:
+    st.write("Secret Content")
+    st.write(st.user)
+    if st.checkbox("Show tokens (not safe!)"):
+        st.write(st.user.tokens)
+
+    st.button("Logout", on_click=st.logout)
+
